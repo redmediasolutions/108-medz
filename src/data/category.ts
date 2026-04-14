6d76b83be86ea765100df98f1c914424dfb7190e
redmediasolutions <redmediasolutions8@gmail.com>
2026-04-14 14:42:27 +0530

    Update category.ts

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,8 +1,4 @@
-export type Category = {
-  id: string;
-  title1: string;
-  title2: string;
-  icon: string;
-  background: string;
-  order: number;
-};
+export const categories = [
+  { id: 1, name: "Pain Relief" },
+  { id: 2, name: "Diabetes" },
+];
